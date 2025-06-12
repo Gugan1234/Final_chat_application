31892ecb2e5c65a728cdce4e9bb126f4627975bf
GUGAN.R <144809786+Gugan1234@users.noreply.github.com>
2025-06-12 14:08:32 +0530

    App.jsx

--- frontend_1/src/App.jsx
+++ frontend_1/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     const text = input.trim();
     if (!text) return;
 
-    const res = await fetch('http://localhost:5000/send_message', {
+    const res = await fetch(' https://final-chat-application-8g6i.onrender.com/send_message', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message: text })
